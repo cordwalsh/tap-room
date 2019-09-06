9fb03731d89ee346a062beada7823c443e35e5c4
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-09-06 16:31:12 -0700

    form backend set, not storing info

--- src/components/Keglist.jsx
+++ src/components/Keglist.jsx
@@ -1,9 +1,5 @@
-// shows a list of all kegs, name, brand, price, alcohol content.
-
 // color or icon code to show if it has less than 10 pints
-
 // kegs are color coded based on alcohol content
-
 // display stronger beers differently than weaker beers
 
 
